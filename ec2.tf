1ae6effce51698d756c1669e10a047127b229706
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-10 21:50:19 -0400

    simple script to create an ec2 instance

---
+++ ec2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  profile    = "terraform"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+  # vpc_security_group_ids = ["sg-0077..."]
+  # subnet_id = "subnet-923a..."
+}
