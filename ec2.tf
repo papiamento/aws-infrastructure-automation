57325c46925b9f6e9facb91fef1c901825df3396
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-11 11:43:33 -0400

    adding ssh key name  & security group

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,23 @@ resource "aws_instance" "example" {
   count	        = 0
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+  
+  key_name = "My Private Key"
   # vpc_security_group_ids = ["sg-0077..."]
   # subnet_id = "subnet-923a..."
+
+  tags {
+    "owner" = "terraform"
+  }
+
+  vpc_security_group_ids = ["sg-0d9a3a138f6746c16"]
+}
+
+resource "security_group" "MyDMV" {
+  
+
+
+  tags {
+    "owner" = "terraform"
+  }
 }
