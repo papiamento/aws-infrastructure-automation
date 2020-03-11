cd118a3e1892a6c713096fcfdfd916a0e47cf33b
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-11 10:05:33 -0400

    delete ec2 instance

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-#  profile    = "terraform"
   region     = "us-east-1"
 }
 
 resource "aws_instance" "example" {
+  count	        = 0
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
   # vpc_security_group_ids = ["sg-0077..."]
