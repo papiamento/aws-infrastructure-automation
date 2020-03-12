d6a4b368deb8622ea61a3d7e4ab339e581c5b58c
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-12 11:35:02 -0400

    create two webservers

--- web_server.tf
+++ web_server.tf
@@ -1,9 +1,11 @@
-resource "aws_instance" "web-server" {
-  count	        = 1
+resource "aws_instance" "webserver1" {
+#  count	        = 1
   ami           = "ami-0a887e401f7654935"
   instance_type = "t2.micro"
   
   key_name = "ExampKP"
+
+  availability_zone = "us-east-1c"
 
   vpc_security_group_ids = ["sg-0d9a3a138f6746c16"]
 
@@ -14,8 +16,32 @@ resource "aws_instance" "web-server" {
     }
 }
 
-output "aws_instance_ip" {
+resource "aws_instance" "webserver2" {
+#  count	        = 1
+  ami           = "ami-0a887e401f7654935"
+  instance_type = "t2.micro"
+  
+  key_name = "ExampKP"
 
-  value = "${aws_instance.web-server[0].public_ip}"
+  availability_zone = "us-east-1b"
+
+  vpc_security_group_ids = ["sg-0d9a3a138f6746c16"]
+
+    user_data = "${file("install_apache.sh")}"
+
+    tags = {
+        owner = "terraform"
+    }
+}
+
+output "webserver1" {
+
+  value = "webserver 1: ${aws_instance.webserver1.public_ip}"
+  description = "The public IP address of our instance"
+}
+
+output "webserver2" {
+
+  value = "webserver 2: ${aws_instance.webserver2.public_ip}"
   description = "The public IP address of our instance"
 }
